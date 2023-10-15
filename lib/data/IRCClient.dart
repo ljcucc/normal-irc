64fde3b018b250382c451db1059c4b8581a908f6
ljcucc <ljcucc@gmail.com>
2023-10-15 19:17:11 +0800

    construct basic layout

--- lib/data/IRCClient.dart
+++
@@ -1,9 +0,0 @@
-import 'package:irc/client.dart';
-
-void connect() {
-  var config = Configuration(
-    host: "irc.ljcu.cc",
-    port: 5023,
-  );
-  var client = new Client(config);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:normal_irc/pages/ChatLogPage.dart';
-import 'package:normal_irc/pages/ChatNavigationPage.dart';
-import 'package:normal_irc/pages/MemberList.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,39 +12,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final themeData = ThemeData(
-      colorScheme: ColorScheme.fromSwatch(
-          primaryColorDark: Colors.black,
-          backgroundColor: Colors.white,
-          accentColor: Colors.black),
-      textTheme: GoogleFonts.spaceGroteskTextTheme(textTheme).copyWith(
-        titleLarge: GoogleFonts.spaceGrotesk(
-          fontSize: 24,
-          fontWeight: FontWeight.w500,
-          letterSpacing: -1,
-        ),
-        bodyLarge: GoogleFonts.spaceGrotesk(
-          fontSize: 24,
-          fontWeight: FontWeight.w500,
-          letterSpacing: -2,
-        ),
-        bodyMedium: GoogleFonts.spaceGrotesk(
-          fontSize: 16,
-          letterSpacing: 0,
-          fontWeight: FontWeight.w500,
-        ),
-        bodySmall: GoogleFonts.spaceGrotesk(
-          fontSize: 12,
-          letterSpacing: 0,
-          fontWeight: FontWeight.w500,
-        ),
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Color.fromARGB(0, 0, 0, 0),
       ),
       useMaterial3: true,
     );
 
     return MaterialApp(
-      title: 'Normal IRC',
+      title: '',
       theme: themeData,
-      home: const MyHomePage(title: '## CHAT'),
+      home: const MyHomePage(title: ''),
     );
   }
 }
@@ -63,60 +37,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return OrientationBuilder(builder: (context, orientation) {
-      return LayoutBuilder(builder: (context, constraints) {
-        var count = 1;
-        // layout based on screen size ( if landscape )
-        if (constraints.maxWidth < 750) {
-          count = 1;
-        } else if (constraints.maxWidth < 1200) {
-          count = 2;
-        } else {
-          count = 3;
-        }
-        // overwrite layout based on orientation
-        if (orientation == Orientation.portrait) {
-          count = 1;
-        }
-
-        const splitLine = BoxDecoration(
-          border: Border(left: BorderSide(width: 1.5, color: Colors.black26)),
-        );
-
-        var layouts = [
-          Flexible(
-            flex: 20,
-            child: Container(
-              constraints: count == 1
-                  ? null
-                  : BoxConstraints(maxWidth: 400, minWidth: 100),
-              child: ChatNavigationPage(),
-            ),
-          ),
-          Flexible(
-            flex: 45,
-            child: Container(
-              // decoration: splitLine,
-              child: ChatLogPage(),
-            ),
-          ),
-          Flexible(
-            flex: 15,
-            child: Container(
-              constraints: BoxConstraints(maxWidth: 400, minWidth: 333),
-              // decoration: splitLine,
-              child: MemberListPage(),
-            ),
-          )
-        ];
-        return Scaffold(
-          body: Container(
-            child: Row(
-              children: layouts.sublist(0, count),
-            ),
-          ),
-        );
-      });
-    });
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Normal IRC"),
+      ),
+      body: Container(),
+    );
   }
 }
